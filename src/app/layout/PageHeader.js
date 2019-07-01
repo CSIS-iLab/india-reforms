0bb2520988144421d735f489b35ecfe7316d1808
leezaweb <leeza.web@gmail.com>
2019-07-01 14:34:31 -0400

    proper alt tag

--- src/app/layout/PageHeader.js
+++ src/app/layout/PageHeader.js
@@ -27,8 +27,7 @@ export default class PageHeader extends React.Component {
                   sizes="(max-width: 320px) 320px,(max-width: 375px) 335px,(max-width: 480px) 440px,(max-width: 675px) 635px,900px"
                   srcset="https://res.cloudinary.com/csisideaslab/image/upload/f_auto,q_70,w_320/v1561989507/india-reforms/Prime_Minister_Modi_cabinet_President_Kovind.jpg  320w,https://res.cloudinary.com/csisideaslab/image/upload/f_auto,q_70,w_335/v1561989507/india-reforms/Prime_Minister_Modi_cabinet_President_Kovind.jpg  335w,https://res.cloudinary.com/csisideaslab/image/upload/f_auto,q_70,w_480/v1561989507/india-reforms/Prime_Minister_Modi_cabinet_President_Kovind.jpg  480w,https://res.cloudinary.com/csisideaslab/image/upload/f_auto,q_70,w_635/v1561989507/india-reforms/Prime_Minister_Modi_cabinet_President_Kovind.jpg  635w,https://res.cloudinary.com/csisideaslab/image/upload/f_auto,w_900/v1561989507/india-reforms/Prime_Minister_Modi_cabinet_President_Kovind.jpg  900w"
                   src="https://res.cloudinary.com/csisideaslab/image/upload/v1561989507/india-reforms/Prime_Minister_Modi_cabinet_President_Kovind.jpg"
-                  title="alt text"
-                  alt="Sample"
+                  alt="Prime Minister Narendra Modi and cabinet ministers stand with President of India Ram Nath Kovind after taking the oath of office at the President house in New Delhi on May 30, 2019."
                 />
               </figure>
               <figcaption class="caption">
