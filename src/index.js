b7467cde0586fe821993aabd207b8f868dcab6b6
leezaweb <leeza.web@gmail.com>
2019-07-15 10:23:53 -0400

    add react snapshot and schema script

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ import './assets/scss/main.scss'
 
 import * as serviceWorker from './serviceWorker'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './app/App'
+import { render } from 'react-snapshot'
 
-ReactDOM.render(
+render(
   <Router>
     <App />
   </Router>,
