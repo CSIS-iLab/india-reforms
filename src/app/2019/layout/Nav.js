e666595c2e757fd2ffeefc8ceb2e294080ccf014
jdromero88 <jdromerov88@gmail.com>
2024-06-03 16:17:19 -0400

    Fix bug social share icons on homepage2019

--- src/app/2019/layout/Nav.js
+++ src/app/2019/layout/Nav.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { ReactComponent as LogoLong } from '../../../assets/images/csis_logo-long.svg'
 import { ReactComponent as LogoLong } from '../../../assets/images/csis_logo-long.svg'
 import { ReactComponent as LogoShort } from '../../../assets/images/csis_logo-short.svg'
 import SocialShare from '../components/SocialShare'
@@ -17,10 +16,16 @@ export default class Nav extends React.Component {
       ),
       homepage: (
         <React.Fragment>
-          <SocialShare page={page} />
+          <SocialShare />
           <LogoShort className="link-home__logo-short" />
         </React.Fragment>
-      )
+      ),
+      homepage2019: (
+        <React.Fragment>
+          <SocialShare />
+          <LogoShort className="link-home__logo-short" />
+        </React.Fragment>
+      ),
     }
 
     return (
@@ -34,7 +39,7 @@ export default class Nav extends React.Component {
 
         <div className="link-home link-home--mobile nav-link">
           <span>India's Economic Reform Agenda</span>
-          <span>A Scorecard</span>{' '}
+          <span>A Scorecard</span>
         </div>
 
         {headerContent[page]}
