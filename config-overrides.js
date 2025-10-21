bb426f1c9cc6d0ae90089972aeceb77ce7d232f9
jdromero88 <jdromerov88@gmail.com>
2025-10-21 15:54:46 -0400

    Update packages

--- config-overrides.js
+++ config-overrides.js
@@ -15,31 +15,31 @@ module.exports = function override(config, env) {
     loader: 'file-loader?name=[name].[ext]'
   })
 
-  config.module.rules.push({
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: [
-      {
-        loader: 'babel-loader',
+  // config.module.rules.push({
+  //   test: /\.(js|jsx)$/,
+  //   exclude: /node_modules/,
+  //   use: [
+  //     {
+  //       loader: 'babel-loader',
 
-        options: {
-          presets: [
-            '@babel/preset-react',
-            {
-              plugins: ['@babel/plugin-proposal-class-properties']
-            }
-          ]
-        }
-      },
-      {
-        loader: 'eslint-loader'
-      }
-    ]
-  })
+  //       options: {
+  //         presets: [
+  //           '@babel/preset-react',
+  //           {
+  //             plugins: ['@babel/plugin-proposal-class-properties']
+  //           }
+  //         ]
+  //       }
+  //     },
+  //     {
+  //       loader: 'eslint-loader'
+  //     }
+  //   ]
+  // })
 
   config.module.rules.push({ test: /\.html$/, loader: 'html-loader' })
 
-  config.plugins.push(new StyleLintPlugin({ fix: true }))
+  config.plugins.push(new StyleLintPlugin({ fix: false }))
 
   return config
 }
